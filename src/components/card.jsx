80a2d19cfbd43375e639e271719cad483883772d
DonJuan <armvnov1ch@gmail.com>
2024-02-20 23:58:42 +0600

    fetch card style

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";const Card = ({ ImagePath, title, description, price, volume }) => {
+import React, { useState } from "react";const Card = ({ imagePath, title, description, price, volume }) => {
    const [isFavorite, setIsFavorite] = useState(false);
 
    const toggleFavorite = () => {
@@ -9,7 +9,7 @@ import React, { useState } from "react";const Card = ({ ImagePath, title, descri
       <div id="card">
          <div className={`card ${isFavorite ? "favorite" : ""}`}>
             {/* <img src={process.env.PUBLIC_URL + `/${ImagePath}`} alt="" /> */}
-            <img src={`${ImagePath}`} alt="" />
+            <img src={imagePath} alt="" />
             <div className="container">
                <h3>{title}</h3>
                <p className="description">{description}</p>
@@ -17,9 +17,6 @@ import React, { useState } from "react";const Card = ({ ImagePath, title, descri
                   <p className="price">{price}</p>
                   <p className="volume">{volume}</p>
                </div>
-               {/* <button className="favorite-button" onClick={toggleFavorite}>
-                  <span className="heart-icon">&#10084;</span>
-               </button> */}
             </div>
          </div>
       </div>
--- src/components/catalog-page.jsx
+++ src/components/catalog-page.jsx
@@ -1,46 +1,50 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Card from "./card";
+import Card from "./card"; // Corrected import statement
 
 const FullCatalog = () => {
-  const [cardsData, setCardsData] = useState([]);
+   const [cardsData, setCardsData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://127.0.0.1:8000/api/shop/perfums/");
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        const fetchedData = await response.json();
-        setCardsData(fetchedData);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+   useEffect(() => {
+      const fetchData = async () => {
+         try {
+            const response = await fetch(
+               "http://127.0.0.1:8000/api/shop/perfums/"
+            );
+            if (!response.ok) {
+               throw new Error("Failed to fetch data");
+            }
+            const fetchedData = await response.json();
+            setCardsData(fetchedData);
 
-    fetchData();
-  }, []);
+            console.log("Fetched Data:", fetchedData);
+         } catch (error) {
+            console.error("Error fetching data:", error);
+         }
+      };
 
-  return (
-    <div id="catalog">
-      <div className="container">
-        <h1 id="title">Каталог</h1>
-        <div className="catalog" id="full">
-          {cardsData.map((card, index) => (
-            <Card
-              key={index}
-              ImagePath={card.ImagePath}
-              title={card.title}
-              description={card.description}
-              price={card.price}
-              volume={card.volume}
-            />
-          ))}
-        </div>
+      fetchData();
+   }, []);
+
+   return (
+      <div id="catalog">
+         <div className="container">
+            <h1 id="title">Каталог</h1>
+            <div className="catalog" id="full">
+               {cardsData.map((card, index) => (
+                  <Card
+                     key={index}
+                     imagePath={card.image}
+                     title={card.name}
+                     description={card.description}
+                     price={card.price}
+                     volume={card.quantity}
+                  />
+               ))}
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default FullCatalog;
