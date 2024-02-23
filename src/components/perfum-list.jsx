7988b37f3f94ec0329c06b50fe987b390d1a7852
DonJuan <armvnov1ch@gmail.com>
2024-02-23 18:08:54 +0600

    added filter

---
+++ src/components/perfum-list.jsx
@@ -0,0 +1,140 @@
+import React, { useState, useEffect } from "react";import Card from "./card";
+const PerfumeList = () => {
+   const [perfumes, setPerfumes] = useState([]);
+   const [filteredPerfumes, setFilteredPerfumes] = useState([]);
+   const [intensiveFilter, setIntensiveFilter] = useState(null);
+   const [genderFilter, setGenderFilter] = useState(null);
+   const [typeFilter, setTypeFilter] = useState(null);
+   const [notesFilter, setNotesFilter] = useState(null);
+   const [brandFilter, setBrandFilter] = useState(null);
+
+   useEffect(() => {
+      // Fetch data from the API endpoint
+      fetch("http://127.0.0.1:8000/api/shop/perfums/")
+         .then((response) => response.json())
+         .then((data) => {
+            setPerfumes(data);
+            setFilteredPerfumes(data);
+         })
+         .catch((error) => console.error("Error fetching data:", error));
+   }, []);
+
+   useEffect(() => {
+      // Apply filters
+      let filteredData = perfumes;
+
+      if (intensiveFilter !== null) {
+         filteredData = filteredData.filter(
+            (item) => item.intensive_category === intensiveFilter
+         );
+      }
+
+      if (genderFilter !== null) {
+         filteredData = filteredData.filter(
+            (item) => item.gender_category === genderFilter
+         );
+      }
+
+      if (typeFilter !== null) {
+         filteredData = filteredData.filter(
+            (item) => item.type_category === typeFilter
+         );
+      }
+
+      if (notesFilter !== null) {
+         filteredData = filteredData.filter(
+            (item) => item.note_category === notesFilter
+         );
+      }
+
+      if (brandFilter !== null) {
+         filteredData = filteredData.filter(
+            (item) => item.brand_category === brandFilter
+         );
+      }
+
+      setFilteredPerfumes(filteredData);
+   }, [
+      intensiveFilter,
+      genderFilter,
+      typeFilter,
+      notesFilter,
+      brandFilter,
+      perfumes,
+   ]);
+
+   return (
+      <div className="container">
+         <h1>Perfume List</h1>
+
+         <div>
+            <label>
+               Gender Category:
+               <select
+                  onChange={(e) => setGenderFilter(parseInt(e.target.value))}
+               >
+                  <option value={null}>All</option>
+                  <option value={"male"}>male</option>
+                  <option value={"female"}>female</option>
+               </select>
+            </label>
+
+            <label>
+               Type Category:
+               <select
+                  onChange={(e) => setTypeFilter(parseInt(e.target.value))}
+               >
+                  <option value={null}>All</option>
+                  <option value={1}>1</option>
+                  <option value={2}>2</option>
+               </select>
+            </label>
+            <label>
+               Intensive Category:
+               <select
+                  onChange={(e) => setIntensiveFilter(parseInt(e.target.value))}
+               >
+                  <option value={null}>All</option>
+                  <option value={1}>Intensive Category 1</option>
+                  <option value={2}>Intensive Category 2</option>
+               </select>
+            </label>
+            <label>
+               Notes Category:
+               <select
+                  onChange={(e) => setNotesFilter(parseInt(e.target.value))}
+               >
+                  <option value={null}>All</option>
+                  <option value={1}>1</option>
+                  <option value={2}>2</option>
+               </select>
+            </label>
+            <label>
+               Brand Category:
+               <select
+                  onChange={(e) => setBrandFilter(parseInt(e.target.value))}
+               >
+                  <option value={null}>All</option>
+                  <option value={1}>1</option>
+                  <option value={2}>2</option>
+               </select>
+            </label>
+         </div>
+
+         <div className="catalog">
+            {filteredPerfumes.map((card, index) => (
+               <Card
+                  key={index}
+                  imagePath={card.image}
+                  title={card.name}
+                  description={card.description}
+                  price={card.price}
+                  volume={card.quantity}
+               />
+            ))}
+         </div>
+      </div>
+   );
+};
+
+export default PerfumeList;
--- src/layouts/catalog.jsx
+++ src/layouts/catalog.jsx
@@ -1,11 +1,14 @@
-import React from "react";import Filter from "../components/filter";
+import React from "react";
+import Filter from "../components/filter";
 import FullCatalog from "../components/catalog-page";
+import PerfumeList from "../components/perfum-list";
 
 const CatalogLayout = () => {
    return (
       <div>
          {/* <Filter /> */}
-         <FullCatalog />
+         {/* <FullCatalog /> */}
+         <PerfumeList />
       </div>
    );
 };
