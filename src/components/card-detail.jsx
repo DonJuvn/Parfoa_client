90ed40ff7e6bec1316c7de30b98474d5291060af
DonJuan <armvnov1ch@gmail.com>
2024-03-03 23:33:48 +0500

    fixed filter, baseUrl

--- src/components/card-detail.jsx
+++ src/components/card-detail.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { baseUrl } from "./baseUrl";
 import { baseLocalUrl } from "./baseUrl";
 
@@ -8,8 +9,8 @@ const CardDetail = () => {
    const [quantity, setQuantity] = useState(null);
 
    // const apiUrl = `http://127.0.0.1:8000/api/shop/perfums/${id}/`;
-   // const apiUrl = baseUrl + `/api/shop/perfums/${id}/`;
-   const apiUrl = baseLocalUrl + `/api/shop/perfums/${id}/`;
+   const apiUrl = baseUrl + `/api/shop/perfums/${id}/`;
+   // const apiUrl = baseLocalUrl + `/api/shop/perfums/${id}/`;
 
    useEffect(() => {
       // Fetch the details of the perfume using the ID
