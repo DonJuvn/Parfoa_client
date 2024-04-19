536d943b33d5aaadeee3201a76f240b3ed98d040
DonJuan <armvnov1ch@gmail.com>
2024-04-19 20:14:18 +0500

    handle delete

--- src/components/main/navigation.jsx
+++ src/components/main/navigation.jsx
@@ -56,7 +56,7 @@ const Navigation = () => {
 
    const handleDeleteItem = (index) => {
       const updatedCartItems = [...cartItems];
-      updatedCartItems.splice(index, 1); // Remove the item at the specified index
+      updatedCartItems.splice(index-1, 1); // Remove the item at the specified index
       setCartItems(updatedCartItems);
       localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
    };
@@ -212,79 +212,7 @@ const Navigation = () => {
          </div>
 
          {isMenuOpen && (
-            <div id="menu">
-               <div className="container">
-                  <div className="menu">
-                     <div className="open-close">
-                        <button onClick={handleButtonClick}>
-                           <img
-                              src={process.env.PUBLIC_URL + `/burger.svg`}
-                              alt=""
-                           />
-                        </button>
-                        <button onClick={handleButtonClick}>
-                           <img
-                              src={process.env.PUBLIC_URL + `/cancel.svg`}
-                              alt=""
-                           />
-                        </button>
-                     </div>
-                     <div className="navigation-header">
-                        <a
-                           onClick={handleButtonClick}
-                           className="title-header"
-                           href="#about-us"
-                        >
-                           О нас
-                        </a>
-                        <a
-                           onClick={handleButtonClick}
-                           className="title-header"
-                           href="#catalog-section"
-                        >
-                           Каталог
-                        </a>
-                        <a
-                           onClick={handleButtonClick}
-                           className="title-header"
-                           href="#blog"
-                        >
-                           Блог
-                        </a>
-                        <a
-                           onClick={handleButtonClick}
-                           className="title-header"
-                           href="#footer"
-                        >
-                           Контакты
-                        </a>
-                        <div className="adress">
-                           <p className="title-header">Адресс:</p>
-                           <p className="desc-header">Казахстан, Тараз</p>
-                        </div>
-                        <div className="adress">
-                           <p className="title-header">Контакты:</p>
-                           <p className="desc-header">parfoe@gmail.com</p>
-                           <p>
-                              <a
-                                 onClick={handleButtonClick}
-                                 className="desc-header"
-                                 href="tel:+123456789"
-                              >
-                                 +7 707 201 95 05
-                              </a>
-                           </p>
-                        </div>
-                        <div className="logotype">
-                           <img
-                              src={process.env.PUBLIC_URL + `/Logotype.svg`}
-                              alt=""
-                           />
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
+            
          )}
          {isCartOpen && (
             <div id="cart">
